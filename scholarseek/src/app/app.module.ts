c6e3d5a9901ee6a507d6c70c2389a0b28b542e39
Nadhir Dhanu Dwinanda <dhanu_dwinanda@yahoo.com>
2019-04-29 02:21:25 +0700

    Add files via upload

--- scholarseek/src/app/app.module.ts
+++ scholarseek/src/app/app.module.ts
@@ -8,20 +8,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage';
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthenticationService } from './services/Authentication.service';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   entryComponents: [],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthGuard,
+    AuthenticationService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
